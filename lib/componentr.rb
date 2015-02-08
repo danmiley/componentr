0af3a4c330afa93d6d3dd43b5e19d5b7d36f01eb
Dan Miley <dan.miley@gmail.com>
2015-02-08 08:48:18 -0800

    move all arg processing out of script

--- lib/componentr.rb
+++ lib/componentr.rb
@@ -1,6 +1,7 @@
 require "componentr/version"
 require 'componentr/translator'
 require 'json'
+require 'optparse'
 
 module Componentr
   def self.hi(language)
@@ -8,65 +9,73 @@ module Componentr
     translator.hi
   end
   def self.process(options, wargs, input)
-      $stderr.puts "#{options.to_s}  and #{wargs} and #{input}" if options[:passthru] == true
-      wargs['status'] = 'success'
-      return wargs, input
+    $stderr.puts "#{options.to_s}  and #{wargs} and #{input}" if options[:passthru] == true
+    wargs['status'] = 'success'
+    return wargs, input
   end
 
 
   def self.inputr
-options = {}
-OptionParser.new do |opts|
-  opts.banner = "Usage: coomponentr [options]"
+    options = {}
+    OptionParser.new do |opts|
+      opts.banner = "Usage: coomponentr [options]"
 
-  opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
-    options[:verbose] = v
-  end
-  opts.on("-p", "--[no-]passthru", "Passthru Arguments") do |p|
-    options[:passthru] = p
-  end
+      opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+        options[:verbose] = v
+      end
+      opts.on("-p", "--[no-]passthru", "Passthru Arguments") do |p|
+        options[:passthru] = p
+      end
 
-  options[:wargs] = nil 
-  opts.on( '-a', '--wargs JSON', 'Write log to FILE' ) do |json|
-    options[:wargs] = json
-  end
+      options[:wargs] = nil 
+      opts.on( '-a', '--wargs JSON', 'Write log to FILE' ) do |json|
+        options[:wargs] = json
+      end
 
-end.parse!
+    end.parse!
 
-# if any commnand line args are actually prpoper named wargs, emobody themm 
-$stderr.puts "RAWR : optionss #{options}"
-wargs = options[:wargs] rescue nil
-options.delete(:wargs)
+    # if any commnand line args are actually prpoper named wargs, emobody themm 
+    $stderr.puts "RAWR : optionss #{options}"
+    wargs = options[:wargs] rescue nil
+    options.delete(:wargs)
 
-wargs = JSON.parse(wargs) if wargs
-$stderr.puts "RAWR: bJSONIc goodies are #{wargs}, right"
+    wargs = JSON.parse(wargs) if wargs
+    $stderr.puts "RAWR: bJSONIc goodies are #{wargs}, right"
 
-# probe input to see if any wargs are in there
+    # probe input to see if any wargs are in there
 
-#p ARGV
-input = ARGV
+    #p ARGV
+    input = ARGV
 
-net_input = input.map { |i|
-  begin
-    candidate = JSON.parse(i) 
-      $stderr.puts "candidate #{candidate} "
+    net_input = input.map { |i|
+      begin
+        candidate = JSON.parse(i) 
+        $stderr.puts "candidate #{candidate} "
         $stderr.puts "subcandidate #{candidate['wargs']} " rescue nil
-    if candidate && candidate['wargs']
-      $stderr.puts "got one #{candidate}"
-      # merge into wargs
-      candidate.delete('wargs')
+        if candidate && candidate['wargs']
+          $stderr.puts "got one #{candidate}"
+          # merge into wargs
 
-      wargs.merge!(candidate)
-      # if merged, we don't need this in stdin
-      nil
-    end
-  rescue  Exception => e
-    i
+          wargs.merge!(candidate)
+          # if merged, we don't need this in stdin
+          nil
+        end
+      rescue  Exception => e
+        i
+      end
+
+    }.compact # get rid of nils
+
+    return options, wargs, net_input
   end
 
-  }.compact # get rid of nils
+  def self.outputr(wargs, output)
+    puts wargs
+    output.map {|o| puts o }
+  end
 
-      return options, wargs, net_input
+  def self.read_eval_print
+    outputr(*process(*inputr))
   end
 
 end
